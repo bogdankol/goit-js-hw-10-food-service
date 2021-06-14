00e8817a93fe13caa9d69b17642f72339d023955
bogdankol <bogdank208@gmail.com>
2021-06-14 10:21:29 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './sass/main.scss';
+// for menu
+import template from './templates/menu-cards.hbs';
+import data from './json/menu.json';
+// for theme changer
+import * as toolbar from './js/toolbar.js';
+
+// menu-cards-creator
+const menuMarkUp = template(data);
+const menu = document.querySelector(`.js-menu`);
+menu.insertAdjacentHTML(`beforeend`, menuMarkUp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/js/toolbar.js
@@ -0,0 +1,104 @@
+
+const refs = {
+  switcher: document.querySelector(`#theme-switch-toggle`),
+  body: document.body
+}
+const {switcher, body} = refs;
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
+
+body.classList.add(localStorage.getItem(`bodyClassCurrent`))
+
+localStorage.getItem(`bodyClassCurrent`) === DARK ?
+  switcher.checked = true
+  : switcher.checked = false;  
+
+switcher.addEventListener(`change`, onChangeHandler);
+
+function onChangeHandler(event) {
+  body.classList.toggle(LIGHT)
+  body.classList.toggle(DARK)
+  getClassData();
+}
+
+function getClassData () {
+  localStorage.setItem(`bodyClassCurrent`, body.classList)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// that's for label click event - we obtain classData from another local key
+
+// const label = document.querySelector(`.theme-switch__track`);
+// label.addEventListener(`click`, onClickHandler);
+// let bodyClassInfo = body.classList;
+
+// if (
+//   localStorage.getItem(`bodyClass`) !== localStorage.getItem(`bodyClassCurrent`) ||
+//   localStorage.getItem(`bodyClass`) === localStorage.getItem(`bodyClassCurrent`)
+// ) {
+//     body.classList.add(localStorage.getItem(`bodyClassCurrent`));
+    
+// } else {
+//   body.classList.add(`LIGHT`);
+// }
+// localStorage.setItem(`bodyClass`, bodyClassInfo);
+
+
+// function onClickHandler(event) {
+//     if (body.classList.contains(LIGHT)) {
+//       body.classList.replace(LIGHT, DARK);
+//       setClass ();
+//     } else {
+//       body.classList.replace(DARK, LIGHT);
+//       setClass ();
+//     } 
+//     bodyClassInfo = body.classList
+// }
+
+// function setClass () {
+//   localStorage.setItem(`bodyClassCurrent`, bodyClassInfo)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
